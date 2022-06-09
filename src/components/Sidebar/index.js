77a86ee605dacd7314f71bcbe2033033170655b4
Calpa <calpaliu@gmail.com>
2022-06-09 14:53:22 -0700

    feat: add recent posts in sidebar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { Grid, Tooltip, Typography } from '@mui/material';
-import { Link } from 'gatsby';
+import {
+  Grid, Tooltip, Typography, Divider,
+} from '@mui/material';
+import { graphql, Link, useStaticQuery } from 'gatsby';
 
 import { StaticImage } from 'gatsby-plugin-image';
 import { palette } from '../../gatsby-theme-material-ui-top-layout/theme';
@@ -10,16 +12,34 @@ const avatarAlt = 'Calpa Liu';
 const width = 120;
 
 function Sidebar(props) {
+  const data = useStaticQuery(graphql`
+  {
+    allMarkdownRemark(sort: {fields: [frontmatter___date], order: DESC}, limit: 10) {
+      edges {
+        node {
+          frontmatter {
+            title
+            slug
+          }
+        }
+      }
+    }
+  }
+`);
+
+  const latestPosts = data.allMarkdownRemark.edges;
+
   return (
     <Grid
       container
       item
       flexDirection="column"
       alignItems="center"
-      xs={2}
+      xs={3}
       sx={{
         padding: '20px',
         marginTop: '20px',
+        background: 'white',
       }}
     >
       <Tooltip title="Nyahello">
@@ -52,6 +72,31 @@ function Sidebar(props) {
       <Typography>
         Nyahello
       </Typography>
+      <Divider
+        sx={{
+          width: '100%',
+          marginTop: '10px',
+          marginBottom: '10px',
+        }}
+      />
+      <Grid container>
+
+        <Typography variant="h3">
+          Recent Posts
+        </Typography>
+        {latestPosts.map(({ node }) => (
+          <Link
+            to={`/blog/${node.frontmatter.slug}`}
+            key={node.frontmatter.slug}
+            style={{
+              textDecoration: 'none',
+              color: palette.blue.main,
+            }}
+          >
+            {node.frontmatter.title}
+          </Link>
+        ))}
+      </Grid>
 
     </Grid>
   );
