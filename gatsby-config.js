4cfbeaee9b8186497cac719e6f2127574f04e14a
Calpa <calpaliu@gmail.com>
2022-12-28 15:28:56 +0800

    feat: use pnpm instead of yarn

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,27 +3,28 @@ module.exports = {
     title: 'Calpa Website',
     siteUrl: 'https://calpa.me/',
   },
-  plugins: ['gatsby-plugin-netlify-cms', 'gatsby-plugin-image', 'gatsby-plugin-react-helmet', 'gatsby-plugin-sitemap', {
+  plugins: ['gatsby-plugin-image', 'gatsby-plugin-react-helmet', 'gatsby-plugin-sitemap', {
     resolve: 'gatsby-plugin-manifest',
     options: {
       icon: 'src/images/icon.jpeg',
     },
   }, 'gatsby-plugin-mdx', 'gatsby-plugin-sharp', 'gatsby-transformer-sharp', {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      name: 'images',
-      path: './src/images/',
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: './src/images/',
+      },
+      __key: 'images',
+    }, {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'content',
+        path: `${__dirname}/src/content`,
+      },
     },
-    __key: 'images',
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      name: 'content',
-      path: `${__dirname}/src/content`,
-    },
-  },
-  'gatsby-transformer-remark',
-  'gatsby-theme-material-ui',
-  'gatsby-plugin-netlify',
+    'gatsby-plugin-pnpm',
+    'gatsby-transformer-remark',
+    'gatsby-theme-material-ui',
+    'gatsby-plugin-netlify',
   ],
 };
